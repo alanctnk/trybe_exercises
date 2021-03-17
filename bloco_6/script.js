5a4d6e024cd3cb66009fd2db0001cbb449678f8c
Alan Tanaka <alanctnk@gmail.com>
2021-03-17 03:16:33 -0300

    Exercícios 6.2

--- bloco_6/script.js
+++ bloco_6/script.js
@@ -3,7 +3,7 @@ let submitBtn = document.getElementById("button_form")
 let clearBtn = document.getElementById("button_clean")
 let requiredFields = document.getElementsByClassName('required')
 let radioFields = document.getElementsByClassName('required_radio')
-
+let data = document.getElementById('data').DatePickerX.init()
 function createEl(tag){
   let element = document.createElement(tag)
   return element
@@ -46,21 +46,13 @@ function validateForm(event) {
   })
 
   // Declarando os booleanos para avaliar na próxima condicional
-  let dataStr = requiredFields[8].value
-  let dataArr = dataStr.split('/')
-  let day = parseInt(dataArr[0])
-  let month = parseInt(dataArr[1])
-  let year = parseInt(dataArr[2])
-  let boolData = (0 < day && day < 31) && (0 < month && month < 12) && (0 < year)
   let boolRadio = radioFields[0].checked || radioFields[1].checked
   let boolCPF = parseInt(requiredFields[2].value)
 
-  if (count === 0 && boolData && boolRadio && boolCPF ){
+  if (count === 0 && boolRadio && boolCPF ){
     alert("Tudo OK!")
   } else {
     alert("Preencha todos os campos corretamente!")
-    let bool = count === 0
-    console.log(boolData)
   }
 }
 
